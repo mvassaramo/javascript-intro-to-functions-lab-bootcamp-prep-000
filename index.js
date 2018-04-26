707e979c5a36413614f29ebff294fb9ef67624be
maduri.vassaramo@hotmail.co.uk <maduri.vassaramo@hotmail.co.uk>
2018-04-26 16:53:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,3 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string)===toLowerCase
-  {
-    console.log("I can\'t hear you!")
-  }
